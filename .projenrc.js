4b8c7db70d636167ec7359bbbc782e819a4ce08c
Pahud <pahudnet@gmail.com>
2021-09-12 22:33:02 +0800

    init commit

--- .projenrc.js
+++ .projenrc.js
@@ -1,16 +1,38 @@
-const { ConstructLibraryCdktf } = require('projen');
-const project = new ConstructLibraryCdktf({
-  author: 'Pahud',
-  authorAddress: 'pahudnet@gmail.com',
-  cdktfVersion: '0.4.0',
-  defaultReleaseBranch: 'main',
-  name: 'cdktf-aws-ecs',
-  repositoryUrl: 'https://github.com/pahudnet/cdktf-aws-ecs.git',
+const { ConstructLibraryCdktf, DependenciesUpgradeMechanism, NpmAccess } = require('projen');
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
-  // release: undefined,      /* Add release management to this project. */
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
+
+const project = new ConstructLibraryCdktf({
+  author: 'Pahud Hsieh',
+  authorAddress: 'pahudnet@gmail.com',
+  cdktfVersion: '0.5.0',
+  defaultReleaseBranch: 'main',
+  name: '@pahud/cdktf-aws-ecs',
+  description: 'CDKTF construct library for Amazon ECS',
+  repositoryUrl: 'https://github.com/pahud/cdktf-aws-ecs',
+  deps: [
+    '@cdktf/provider-aws',
+  ],
+  peerDeps: [
+    '@cdktf/provider-aws',
+  ],
+  minNodeVersion: '12.20.0',
+  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: AUTOMATION_TOKEN,
+    },
+  }),
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['pahud'],
+  },
+  npmAccess: NpmAccess.PUBLIC,
 });
+
+const common_exclude = ['cdktf.out', 'yarn-error.log', 'dependabot.yml', '.terraform', 'terraform.*'];
+project.npmignore.exclude(...common_exclude, 'images', 'docs', 'website');
+project.gitignore.exclude(...common_exclude);
+
 project.synth();
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# replace this
+# cdktf-aws-ecs
+
+CDKTF construct library for Amazon ECS.
+
+## Usage
+
+The following sample creates:
+
+1. A new VPC
+1. Amazon ECS cluster
+2. Autoscaling Group capacity provider
+3. Autoscaling Group with Launch Template
+
+
+```ts
+import { Cluster } from '@pahud/cdktf-aws-ecs';
+
+// create the cluster
+const cluster = new Cluster(stack, 'EcsCluster');
+
+// create the ASG capacity with capacity provider
+cluster.addAsgCapacity('ASGCapacity', {
+  maxCapacity: 10,
+  minCapacity: 0,
+  desiredCapacity: 2,
+});
+```
+
+## Existing VPC subnets
+
+To deploy in any existing VPC, specify the `vpcSubnets.
+
+```ts
+cluster.addAsgCapacity('ASGCapacity', {
+  vpcSubnets: ['subnet-111','subnet-222','subnet-333' ],
+});
+```
