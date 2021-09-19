4ba748a67edb7d8fdfc03b5ed7c56ff50a09b8d1
Pahud Hsieh <pahudnet@gmail.com>
2021-09-19 21:53:27 +0800

    update unit tests (#16)
    Fixes #

--- test/default.test.ts
+++ test/default.test.ts
@@ -1,15 +1,54 @@
+import 'cdktf/lib/testing/adapters/jest';
+import { DataAwsAvailabilityZones, AutoscalingGroup, EcsCapacityProvider } from '@cdktf/provider-aws';
 import { TerraformStack, Testing } from 'cdktf';
 import { Cluster } from '../src';
 
 Testing.setupJest();
-let app = Testing.app();
-let stack = new TerraformStack(app, 'test');
 
 describe('Unit testing using snapshots', () => {
   it('should match the snapshot', () => {
+    const app = Testing.app();
+    const stack = new TerraformStack(app, 'test');
     expect(
       Testing.synthScope(() => {
         new Cluster(stack, 'Cluster');
       })).toMatchSnapshot();
   });
+  /**
+   * TODO - we need `terraform` executable in the github workflow image to run this check.
+   */
+  // it('should plan successfully', () => {
+  //   const app = Testing.app();
+  //   const stack = new TerraformStack(app, 'test');
+  //   expect(Testing.fullSynth(stack)).toPlanSuccessfully();
+  // });
+  it('should have a cluster', () => {
+    expect(Testing.synthScope((scope) => {
+      new Cluster(scope, 'Cluster')
+        .addAsgCapacity('Capacity', {
+          desiredCapacity: 2,
+        });
+    })).toHaveDataSource(DataAwsAvailabilityZones);
+  });
+  it('should have an ASG', () => {
+    expect(Testing.synthScope((scope) => {
+      new Cluster(scope, 'Cluster')
+        .addAsgCapacity('Capacity', {
+          desiredCapacity: 2,
+        });
+    })).toHaveResourceWithProperties(AutoscalingGroup, {
+      desired_capacity: 2,
+      max_size: 2,
+      min_size: 0,
+    });
+  });
+  it('should have a capacity provider', () => {
+    expect(Testing.synthScope((scope) => {
+      new Cluster(scope, 'Cluster')
+        .addAsgCapacity('Capacity', {
+          desiredCapacity: 2,
+        });
+    })).toHaveResource(EcsCapacityProvider);
+  });
 });
+
